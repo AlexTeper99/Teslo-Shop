679776bb0a76ba6935ee388a3ab3916ccfda120a
AlexTeper99 <ateper@fi.uba.ar>
2022-08-01 12:57:03 -0300

    spacing cart css

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -3,21 +3,22 @@ import { Box, Button, Card, CardContent, Divider, Grid, Typography } from '@mui/
 import { ShopLayout } from '../../components/layouts/ShopLayout';
 import { CartList, OrderSummary } from '../../components/cart';
 
+
 const CartPage = () => {
   return (
     <ShopLayout title='Carrito - 3' pageDescription={'Carrito de compras de la tienda'}>
         <Typography variant='h1' component='h1'>Carrito</Typography>
 
-        <Grid container>
+        <Grid container spacing={3}>
             <Grid item xs={ 12 } sm={ 7 }>
                 <CartList editable />
             </Grid>
+            
             <Grid item xs={ 12 } sm={ 5 }>
                 <Card className='summary-card'>
                     <CardContent>
                         <Typography variant='h2'>Orden</Typography>
                         <Divider sx={{ my:1 }} />
-
                         <OrderSummary />
 
                         <Box sx={{ mt: 3 }}>
